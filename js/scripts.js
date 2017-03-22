b72cc69e32a9f34c702a441612f6127e747f667c
Filip Nagalewski <nagalewski@gmail.com>
2017-03-22 14:08:30 +0100

    files added

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// my first js script
+
+var a = 4, // some meaningless comment
+  h = 8,
+  triangleArea = alert(a*h/2);
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+
+/* that's all folks!
+    ...for now */
