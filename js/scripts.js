ebc874090fb2f0bacfb076788a3b4aab58848b71
Filip Nagalewski <nagalewski@gmail.com>
2017-03-23 11:44:46 +0100

    number validation added

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
 var a = prompt('Pole trójkąta o podstawie "a": '),
   h =  prompt('... i wysokości "h": '),
-  triangleArea = alert('... wynosi: ' + a*h/2);
-console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+  triangleArea;
+
+if (!isNaN(Number(a)) && !isNaN(Number(h))) {
+  triangleArea = a*h/2;
+  console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+} else {
+  console.log('Złe dane. Wprowadź cyfry.');
+}
